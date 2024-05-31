aea3b01e1172ab04671e15f5f3a32927d844d6f5
thebamorgan <77521759+thebamorgan@users.noreply.github.com>
2024-05-31 15:23:19 -0500

    Added gh-pages package for website to be shown on GitHub instead of readme

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("00c23cae3b6eca05")
+/******/ 		__webpack_require__.h = () => ("083561059eaafc07")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c4d6e5d40dedf0e5"; }
+/******/ 		__webpack_require__.h = function() { return "20031c15b09ddcfb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
